77d778613d79f5c04f2be78c4efebb32889264b5
Lenghia0183 <lenghia0183@gmail.com>
2024-07-28 13:52:29 +0700

    add flexibleAuth middleware

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -35,6 +35,33 @@ const authorize = (rolesAllow) => (req, res, next) => {
   next();
 };
 
+const flexibleAuth = catchAsync(async (req, res, next) => {
+  const token = extractToken(req);
+
+  if (token) {
+    try {
+      const decoded = jwt.verify(token, env.jwt.secretAccess);
+      const user = await User.findByIdAndUpdate(decoded.id, {
+        lastActive: Date.now(),
+      });
+
+      if (!user) {
+        return next(new ApiError(httpStatus.UNAUTHORIZED, authMessage().INVALID_TOKEN));
+      }
+
+      if (user.isLocked) {
+        return next(new ApiError(httpStatus.FORBIDDEN, authMessage().ACCOUNT_LOCKED));
+      }
+
+      req[REQUEST_USER_KEY] = user;
+    } catch (error) {
+      return next(new ApiError(httpStatus.UNAUTHORIZED, authMessage().INVALID_TOKEN));
+    }
+  }
+
+  next();
+});
+
 const extractToken = (req) => {
   let token;
   if (req.headers.authorization?.startsWith('Bearer')) {
